8b3fb73a8439633d06c497d84e41e854d99ae4ea
Classic Neupane <classicneupane@gmail.com>
2024-10-13 10:46:39 +0545

    chore: add firebase config

--- .playground/firebase.ts
+++ .playground/firebase.ts
@@ -1,8 +1,26 @@
 import { getApps, initializeApp } from 'firebase/app'
 
 const config = {
-  dev: {},
-  main: {}
+  dev: {
+    apiKey: 'AIzaSyC3AIhc1wG-ofnt6s70BcidAXdnNRV5PzE',
+    authDomain: 'freelancer-brian-b.firebaseapp.com',
+    databaseURL: 'https://freelancer-brian-b-default-rtdb.firebaseio.com',
+    projectId: 'freelancer-brian-b',
+    storageBucket: 'freelancer-brian-b.appspot.com',
+    messagingSenderId: '149556519970',
+    appId: '1:149556519970:web:18c6bc034bd61c9049167b',
+    measurementId: 'G-WQ943MB4FW'
+  },
+  main: {
+    apiKey: 'AIzaSyCWTg3cDv3zhqo2QxtDi6wZsrv_YKWh6Sg',
+    authDomain: 'property-analysis-29b2f.firebaseapp.com',
+    databaseURL: 'https://property-analysis-29b2f-default-rtdb.firebaseio.com',
+    projectId: 'property-analysis-29b2f',
+    storageBucket: 'property-analysis-29b2f.appspot.com',
+    messagingSenderId: '482032857621',
+    appId: '1:482032857621:web:dece6b892abaa70114d41c',
+    measurementId: 'G-6RZ7D9MGJ1'
+  }
 } as const
 
 const firebaseConfig = () => {
